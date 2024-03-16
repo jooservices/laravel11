21ed07971556cec10cfa5dc02c509d02cfbc6995
Viet Vu <jooservices@gmail.com>
2024-03-16 10:23:50 +0700

    Update Modules

--- config/modules.php
+++ config/modules.php
@@ -71,7 +71,7 @@ return [
         |
         */
 
-        'modules' => base_path('app/Modules'),
+        'modules' => base_path('Modules'),
         /*
         |--------------------------------------------------------------------------
         | Modules assets path
